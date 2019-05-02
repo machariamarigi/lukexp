dc5b8b5416c57f89cbb7ca2d1dfb58c2fcf9ecd0
machariamarigi <machariamarigi@gmail.com>
2019-05-02 17:19:38 +0300

    Consume data in the HelloWorld component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,102 +1,41 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div>
+    <h1>Welcome To The Forum</h1>
+    <div
+      v-for="thread in threads"
+      v-bind:key="thread.id"
+    >
+      <h1>{{ thread.title }}</h1>
+      <div
+        v-for="postId in thread.posts"
+        v-bind:key="postId"
+      >
+        <p>{{ users[posts[postId].userId].name }}</p>
+        <p>{{ posts[postId].text }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import sourceData from "../data.json";
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
     };
-  },
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
