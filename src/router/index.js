d8bcf07eaa4b90987237fb4ae10517ef55054f49
machariamarigi <machariamarigi@gmail.com>
2019-05-03 01:11:02 +0300

    Create a page for showing a single thread

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
+import ShowThread from '@/components/ShowThread';
 
 Vue.use(Router);
 
@@ -9,7 +10,14 @@ export default new Router({
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld,
+      component: HelloWorld
     },
+    {
+      path: '/thread/:id',
+      name: 'ShowThread',
+      component: ShowThread,
+      props: true
+    }
   ],
+  mode: 'history'
 });
