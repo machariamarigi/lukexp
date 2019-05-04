f8e62fcf3ed29c93b4726b69863eabc76857cb0a
machariamarigi <machariamarigi@gmail.com>
2019-05-04 14:48:41 +0300

    reorganise the components

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
-import ShowThread from '@/components/ShowThread';
+import HomePage from '@/pages/HomePage';
+import ShowThreadPage from '@/pages/ShowThreadPage';
 
 Vue.use(Router);
 
@@ -9,13 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'HomePage',
+      component: HomePage
     },
     {
       path: '/thread/:id',
       name: 'ShowThread',
-      component: ShowThread,
+      component: ShowThreadPage,
       props: true
     }
   ],
