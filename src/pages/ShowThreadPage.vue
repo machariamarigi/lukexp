5b7698d29f0656a1e1dd5c910dbceb90e1d644fd
machariamarigi <machariamarigi@gmail.com>
2019-05-05 18:14:39 +0300

    create form to add new post in a thread

--- src/pages/ShowThreadPage.vue
+++ src/pages/ShowThreadPage.vue
@@ -3,6 +3,21 @@
     <div class="col-large push-top">
       <h1>{{thread.title}}</h1>
       <PostList :posts="posts" />
+      <form @submit.prevent="addPost">
+        <div class="form-group">
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            class="form-input"
+            v-model="newPostText"
+          ></textarea>
+          <div class="form-actions">
+            <button class="btn-blue">Submit Post</button>
+          </div>
+        </div>
+      </form>
     </div>
   </div>
 </template>
@@ -15,16 +30,39 @@ export default {
   components: {
     PostList
   },
+
   props: {
     id: {
       required: true,
       type: String
     }
   },
+
   data() {
     return {
-      thread: sourceData.threads[this.id]
+      thread: sourceData.threads[this.id],
+      newPostText: ""
     };
+  },
+
+  methods: {
+    addPost() {
+      const postId = `newpost ${Math.random()}`;
+
+      const post = {
+        text: this.newPostText,
+        publishedAt: Math.floor(Date.now() / 1000),
+        threadId: this.id,
+        userId: "rpbB8C6ifrYmNDufMERWfQUoa202",
+        ".key": postId
+      };
+
+      this.$set(sourceData.posts, postId, post);
+      this.$set(this.thread.posts, postId, postId);
+      this.$set(sourceData.users[post.userId].posts, postId, postId);
+
+      this.newPostText = "";
+    }
   },
 
   computed: {
