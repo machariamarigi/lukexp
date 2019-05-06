2ea363277cb83baa9fbb37a561dbd0db93854f85
machariamarigi <machariamarigi@gmail.com>
2019-05-06 15:49:51 +0300

    create component BaseDate for formating dates

---
+++ src/components/BaseDate.vue
@@ -0,0 +1,28 @@
+<template>
+  <span :title="timestamp | readableDate">
+    {{ timestamp | timePassed }}
+  </span>
+</template>
+
+<script>
+import moment from "moment";
+
+export default {
+  props: {
+    timestamp: {
+      required: true,
+      type: Number
+    }
+  },
+
+  filters: {
+    readableDate(date) {
+      return moment.unix(date).format("MMMM Do YYYY, h:mm:ss a");
+    },
+    timePassed(date) {
+      return moment.unix(date).fromNow();
+    }
+  }
+};
+</script>
+
