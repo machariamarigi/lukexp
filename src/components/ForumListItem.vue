a2a93b7813b6a9657b29acbd4322d2275b6f5eb5
machariamarigi <machariamarigi@gmail.com>
2019-05-07 00:36:10 +0300

    create ForumList and ForumListItem components

---
+++ src/components/ForumListItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="forum-listing">
+    <div class="forum-details">
+      <router-link
+        href="#"
+        class="forum-name"
+        :to="{name: 'Forum', params: { id: forum['.key']}}"
+      >{{ forum.name }}</router-link>
+
+      <p class="forum-description ">{{ forum.description }}</p>
+    </div>
+
+    <div class="threads-count">
+      <p class="count">{{ threadCount }}</p>
+      {{ threadCount === 1 ? 'thread' : 'threads' }}
+    </div>
+
+    <div class="last-thread">
+      <!-- TODO: Implement later -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    forum: {
+      required: true,
+      type: Object
+    }
+  },
+
+  computed: {
+    threadCount() {
+      return this.forum.threads ? Object.values(this.forum.threads).length : 0;
+    }
+  }
+};
+</script>
+
