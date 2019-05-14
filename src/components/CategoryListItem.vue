b2ce1906fb4a04f768d9810bdca4c9b2baee4d91
machariamarigi <machariamarigi@gmail.com>
2019-05-14 15:18:44 +0300

    add Category List components

---
+++ src/components/CategoryListItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="forum-list">
+    <h2 class="list-title"><a href="#">{{ category.name }}</a></h2>
+
+    <ForumList :forums="categoryForums" />
+  </div>
+</template>
+
+<script>
+import ForumList from "./ForumList";
+import sourceData from "../data.json";
+
+export default {
+  components: {
+    ForumList
+  },
+
+  props: {
+    category: {
+      required: true,
+      type: Object
+    }
+  },
+
+  computed: {
+    categoryForums() {
+      return Object.values(sourceData.forums).filter(
+        forum => forum.categoryId === this.category["._key"]
+      );
+    }
+  }
+};
+</script>
