9b654aad49e844beba791ad7d8bb8b5780bacf3b
machariamarigi <machariamarigi@gmail.com>
2019-05-22 15:45:52 +0300

    add actions and mutations for adding a post

--- src/store/index.js
+++ src/store/index.js
@@ -6,5 +6,30 @@ import sourceData from '../data.json';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: sourceData
+  state: sourceData,
+
+  actions: {
+    createPost({ commit }, post) {
+      const postId = `newpost ${Math.random()}`;
+      // eslint-disable-next-line no-param-reassign
+      post['.key'] = postId
+      commit('setPost', { post, postId });
+      commit('appendPostToThread', { threadId: post.threadId, postId });
+      commit('appendPostToUser', { userId: post.userId, postId });
+    }
+  },
+
+  mutations: {
+    setPost(state, { post, postId }) {
+      Vue.set(state.posts, postId, post);
+    },
+
+    appendPostToThread(state, { threadId, postId }) {
+      Vue.set(state.threads[threadId].posts, postId, postId);
+    },
+
+    appendPostToUser(state, { userId, postId }) {
+      Vue.set(state.users[userId].posts, postId, postId);
+    }
+  }
 });
